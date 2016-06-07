ecc9956a49ae4420ed7f72a3da5bacc831ce6387
Samuel <SCroteau94@gmail.com>
2016-06-07 14:22:33 -0400

    fix!!!

---
+++ LOG121-Lab2/src/trialgorithm/OrdreOriginal.java
@@ -0,0 +1,13 @@
+package trialgorithm;
+
+import structurededonne.CustomLinkedList;
+
+public class OrdreOriginal implements TriStrategy{
+
+	@Override
+	public CustomLinkedList tri(CustomLinkedList arr) {
+		return arr;
+		//git
+	}
+
+}
---
+++ LOG121-Lab2/src/trialgorithm/TriDistDecroissant.java
@@ -0,0 +1,22 @@
+package trialgorithm;
+
+import structurededonne.CustomLinkedList;
+import forme.AbstractForme;
+
+public class TriDistDecroissant implements TriStrategy {
+
+	public CustomLinkedList tri(CustomLinkedList arr) {
+		for(int i=0;i<arr.size()-1;i++){
+			for(int j=0;j<arr.size()-1;j++){
+				AbstractForme current = (AbstractForme)arr.get(j);
+				AbstractForme currentNext = (AbstractForme)arr.get(j+1);
+				
+				if(current.getDistanceMax() < currentNext.getDistanceMax()){
+					arr.swapWithNext(j);
+				}
+			}
+		}
+		
+		return arr;
+	}
+}
